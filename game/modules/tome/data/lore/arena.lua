d0e8e8e756f94a03b8648364dd122bb1e830d0e5
DarkGod <darkgod@net-core.org>
2014-10-17 13:14:48 +0200

    fix arena lore

--- game/modules/tome/data/lore/arena.lua
+++ game/modules/tome/data/lore/arena.lua
@@ -7,14 +7,14 @@ newLore{
 
 #{normal}#The rules of the arena are simple.
 Every wave, the gates will open, releasing enemies. When the gates close, you will be given a bonus score.
-The bonus will #YELLOW#decrease every turn #WHITE#, but #YELLOW#enemies you kill at this point#WHITE#, will
+The bonus will #YELLOW#decrease every turn #LAST#, but #YELLOW#enemies you kill at this point#LAST#, will
 raise your bonus. When you defeat all enemies, you will be granted your bonus score, multiplied by
 your bonus multiplier.
 
 Every few rounds, a miniboss round will come. After two miniboss rounds, one out of four bosses will appear.
-#YELLOW#Kill enemies before the gates close #WHITE#to raise your score multiplier!.
-#YELLOW#Kill several enemies at once or very strong enemies #WHITE#to please the audience and increase your rank!.
-#YELLOW#Collect gold #WHITE#to increase your score and resources at the end of each round.
+#YELLOW#Kill enemies before the gates close #LAST#to raise your score multiplier!.
+#YELLOW#Kill several enemies at once or very strong enemies #LAST#to please the audience and increase your rank!.
+#YELLOW#Collect gold #LAST#to increase your score and resources at the end of each round.
 #YELLOW#Kill enemies fast to get better scores!
 #YELLOW#Take advantage of the trap at the center of the arena!
 Good luck!
