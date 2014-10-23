c1aa5e8885c76b2d30efa9c046bffb70cd5b68f3
DarkGod <darkgod@net-core.org>
2014-10-23 20:19:13 +0200

    pfffff

--- game/modules/tome/class/Actor.lua
+++ game/modules/tome/class/Actor.lua
@@ -872,7 +872,7 @@ function _M:alterMakeMapObject(tiles, mo, z, lastmo)
 	local tex, texx, texy, pos_x, pos_y = tiles:get("", 0, 0, 0, 0, 0, 0, "invis.png", false, false, true)
 	submo:texture(0, tex, false, texx, texy, pos_x, pos_y)
 	submo:chain(mo)
-	if lastmo == mo then lastmo = submo end
+	-- if lastmo == mo then lastmo = submo end
 	mo = submo
 	self._mo = mo
 
