4f0ef6978392ec9067af138cd615023a67c8feb9
Shibari <ShibariTOME@Gmail.com>
2014-12-04 08:41:04 -0500

    - Removed all rares/uniques/randbosses from the forced start zones using the Searing Halls method
    - Removed the instakill and rank changes for higher difficulties to normalize the ruleset

--- game/modules/tome/data/zones/blighted-ruins/zone.lua
+++ game/modules/tome/data/zones/blighted-ruins/zone.lua
@@ -48,7 +48,8 @@ return {
 			door = "DOOR",
 		},
 		actor = {
-			class = "mod.class.generator.actor.Random",
+			class = "engine.generator.actor.Random",
+			--class = "mod.class.generator.actor.Random",
 			nb_npc = {20, 30},
 			filters = { {max_ood=2}, },
 			guardian = "HALF_BONE_GIANT", guardian_level = 1,
