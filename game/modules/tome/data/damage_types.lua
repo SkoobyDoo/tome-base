b4e8e34ae20127e2fdb0dfb3abe553c3d4964fe6
Alexander Sedov <alex0player@gmail.com>
2014-12-15 22:49:38 +0300

    Fixed callbackOnDealDamage

--- game/modules/tome/data/damage_types.lua
+++ game/modules/tome/data/damage_types.lua
@@ -404,11 +404,14 @@ setDefaultProjector(function(src, x, y, type, dam, tmp, no_martyr)
 
 		local hd = {"DamageProjector:final", src=src, x=x, y=y, type=type, dam=dam, tmp=tmp, no_martyr=no_martyr}
 		if src:triggerHook(hd) then dam = hd.dam if hd.stopped then return hd.stopped end end
-		if target.fireTalentCheck then
-			local ret = target:fireTalentCheck("callbackOnTakeDamage", src, x, y, type, dam, tmp, no_martyr)
-			if ret then
-				if ret.dam then dam = ret.dam end
-				if ret.stopped then return ret.stopped end
+		if target.listCallbacks then
+			local cbs = target:listCallbacks("callbackOnTakeDamage")
+			for _, cb in ipairs(cbs) do
+				local ret = cb(src, x, y, type, dam, tmp, no_martyr)
+				if ret then
+					if ret.dam then dam = ret.dam end
+					if ret.stopped then return ret.stopped end
+				end
 			end
 		end
 
