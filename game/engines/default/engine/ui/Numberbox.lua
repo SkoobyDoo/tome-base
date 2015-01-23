eee8e2649f9eb75bf473c52bb9a7e2a8fdcfd56d
Alexander Sedov <alex0player@gmail.com>
2015-01-24 02:43:54 +0300

    Big Numberbox update (yes, yes, I tested it, it actually fixes an old bug on my system)

--- game/engines/default/engine/ui/Numberbox.lua
+++ game/engines/default/engine/ui/Numberbox.lua
@@ -18,29 +18,24 @@
 -- darkgod@te4.org
 
 require "engine.class"
-local Base = require "engine.ui.Base"
-local Focusable = require "engine.ui.Focusable"
+local Textbox = require "engine.ui.Textbox"
 
 --- A generic UI textbox
-module(..., package.seeall, class.inherit(Base, Focusable))
+module(..., package.seeall, class.inherit(Textbox))
 
 function _M:init(t)
-	self.title = assert(t.title, "no numberbox title")
+	t = table.clone(t)
+	local on_change = t.on_change
+	t.on_change = nil
+	if t.number then
+		t.text = tostring(t.number)
+	end
+	Textbox.init(self, t)
 	self.number = t.number or 0
+	self.on_number_change = on_change
 	self.min = t.min or 0
-	self.on_change = t.on_change
 	self.max = t.max or 9999999
-	self.fct = assert(t.fct, "no numberbox fct")
-	self.chars = assert(t.chars, "no numberbox chars")
 	self.first = true
-
-	self.tmp = {}
-	local text = tostring(self.number)
-	for i = 1, #text do self.tmp[#self.tmp+1] = text:sub(i, i) end
-	self.cursor = #self.tmp + 1
-	self.scroll = 1
-
-	Base.init(self, t)
 end
 
 function _M:on_focus(v)
@@ -48,81 +43,14 @@ function _M:on_focus(v)
 end
 
 function _M:generate()
-	self.mouse:reset()
-	self.key:reset()
+	Textbox.generate(self)
 
-	-- Draw UI
-	local title_w = self.font:size(self.title)
-	self.title_w = title_w
-	local frame_w = self.chars * self.font_mono_w + 12
-	self.w = title_w + frame_w
-	self.h = self.font_h + 6
+	self.key:addIgnore("_UP", false)
+	self.key:addIgnore("_DOWN", false)
 
-	self.texcursor = self:getUITexture("ui/textbox-cursor.png")
-	self.frame = self:makeFrame("ui/textbox", frame_w, self.h)
-	self.frame_sel = self:makeFrame("ui/textbox-sel", frame_w, self.h)
-
-	local w, h = self.w, self.h
-	local fw, fh = frame_w - 12, self.font_h
-	self.fw, self.fh = fw, fh
-	self.text_x = 6 + title_w
-	self.text_y = (h - fh) / 2
-	self.cursor_y = (h - self.texcursor.h) / 2
-	self.max_display = math.floor(fw / self.font_mono_w)
-	self.text_surf = core.display.newSurface(fw, fh)
-	self.text_tex, self.text_tex_w, self.text_tex_h = self.text_surf:glTexture()
-	self:updateText()
-
-	if title_w > 0 then
-		local s = core.display.newSurface(title_w, h)
-		s:erase(0, 0, 0, 0)
-		s:drawColorStringBlended(self.font, self.title, 0, (h - fh) / 2, 255, 255, 255, true)
-		self.tex, self.tex_w, self.tex_h = s:glTexture()
-	end
-
-	-- Add UI controls
-	self.mouse:registerZone(title_w + 6, 0, fw, h, function(button, x, y, xrel, yrel, bx, by, event)
-		if event == "button" then
-			self.cursor = util.bound(math.floor(bx / self.font_mono_w) + self.scroll, 1, #self.tmp+1)
-			self:updateText()
-		end
-	end)
-	self.key:addBind("ACCEPT", function() self.first = false self.fct(self.text) end)
-	self.key:addIgnore("_ESCAPE", v)
-	self.key:addIgnore("_TAB", true)
 	self.key:addCommands{
 		_UP = function() self.first = false self:updateText(1) end,
 		_DOWN = function() self.first = false self:updateText(-1) end,
-		_LEFT = function() self.first = false self.cursor = util.bound(self.cursor - 1, 1, #self.tmp+1) self.scroll = util.scroll(self.cursor, self.scroll, self.max_display) self:updateText() end,
-		_RIGHT = function() self.first = false self.cursor = util.bound(self.cursor + 1, 1, #self.tmp+1) self.scroll = util.scroll(self.cursor, self.scroll, self.max_display) self:updateText() end,
-		_DELETE = function()
-			if self.first then self.first = false self.tmp = {} self:updateText() end
-			if self.cursor <= #self.tmp then
-				table.remove(self.tmp, self.cursor)
-				self:updateText()
-			end
-		end,
-		_BACKSPACE = function()
-			if self.first then self.first = false self.tmp = {} self:updateText() end
-			if self.cursor > 1 then
-				table.remove(self.tmp, self.cursor - 1)
-				self.cursor = self.cursor - 1
-				self.scroll = util.scroll(self.cursor, self.scroll, self.max_display)
-				self:updateText()
-			end
-		end,
-		_HOME = function()
-			self.first = false
-			self.cursor = 1
-			self.scroll = util.scroll(self.cursor, self.scroll, self.max_display)
-			self:updateText()
-		end,
-		_END = function()
-			self.first = false
-			self.cursor = #self.tmp + 1
-			self.scroll = util.scroll(self.cursor, self.scroll, self.max_display)
-			self:updateText()
-		end,
 		__TEXTINPUT = function(c)
 			if self.first then self.first = false self.tmp = {} self.cursor = 1 end
 			if #self.tmp and (c == '-' or c == '0' or c == '1' or c == '2' or c == '3' or c == '4' or c == '5' or c == '6' or c == '7' or c == '8' or c == '9') then
@@ -137,46 +65,16 @@ end
 
 function _M:updateText(v)
 	local old = self.number
-	local text = ""
 	if not v then
-		if not self.tmp[1] then self.tmp = {} end
-		self.number = tonumber(table.concat(self.tmp)) or 0
+		self.number = self.tmp and tonumber(table.concat(self.tmp)) or 0
 		self.number = util.bound(self.number, self.min, self.max)
-		for i = self.scroll, self.scroll + self.max_display - 1 do
-			if not self.tmp[i] then break end
-			text = text .. self.tmp[i]
-		end
+		Textbox.updateText(self)
 	else
 		self.number = self.number or 0
 		self.number = util.bound(self.number + v, self.min, self.max)
 		text = tostring(self.number)
-		self.tmp = {}
-		for i = 1, #text do self.tmp[#self.tmp+1] = text:sub(i, i) if not self.tmp[#self.tmp] then break end end
-		self.cursor = #self.tmp + 1
+		Textbox.setText(self, text)
 	end
 
-	self.text_surf:erase(0, 0, 0, 0)
-	self.text_surf:drawStringBlended(self.font_mono, text, 0, 0, 255, 255, 255, true)
-	self.text_surf:updateTexture(self.text_tex)
-	if self.on_change and old ~= self.number then self.on_change(self.number) end
-end
-
-function _M:display(x, y, nb_keyframes)
-	if self.tex then
-		if self.text_shadow then self.tex:toScreenFull(x+1, y+1, self.title_w, self.h, self.tex_w, self.tex_h, 0, 0, 0, self.text_shadow) end
-		self.tex:toScreenFull(x, y, self.title_w, self.h, self.tex_w, self.tex_h)
-	end
-	if self.focused then
-		self:drawFrame(self.frame_sel, x + self.title_w, y)
-		self.texcursor.t:toScreenFull(x + self.text_x + (self.cursor-self.scroll) * self.font_mono_w - (self.texcursor.w / 2), y + self.cursor_y, self.texcursor.w, self.texcursor.h, self.texcursor.tw, self.texcursor.th)
-	else
-		self:drawFrame(self.frame, x + self.title_w, y)
-		if self.focus_decay then
-			self:drawFrame(self.frame_sel, x + self.title_w, y, 1, 1, 1, self.focus_decay / self.focus_decay_max_d)
-			self.focus_decay = self.focus_decay - nb_keyframes
-			if self.focus_decay <= 0 then self.focus_decay = nil end
-		end
-	end
-	if self.text_shadow then self.text_tex:toScreenFull(x+1 + self.text_x, y+1 + self.text_y, self.fw, self.fh, self.text_tex_w, self.text_tex_h, 0, 0, 0, self.text_shadow) end
-	self.text_tex:toScreenFull(x + self.text_x, y + self.text_y, self.fw, self.fh, self.text_tex_w, self.text_tex_h)
+	if self.on_change and old ~= self.number then self.on_number_change(self.number) end
 end
