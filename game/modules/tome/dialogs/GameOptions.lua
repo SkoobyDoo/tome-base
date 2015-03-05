5dc016a9f7a6f8dfe078495bb3329d12afd101d0
DarkGod <darkgod@net-core.org>
2015-03-05 19:10:11 +0100

    fix autotarget option display

--- game/modules/tome/dialogs/GameOptions.lua
+++ game/modules/tome/dialogs/GameOptions.lua
@@ -463,7 +463,7 @@ function _M:generateListGameplay()
 
 	local zone = Textzone.new{width=self.c_desc.w, height=self.c_desc.h, text=string.toTString"Auto-validate targets. If you fire an arrow/talent/... it will automatically use the default target without asking\n#LIGHT_RED#This is dangerous. Do not enable unless you know exactly what you are doing.#WHITE#\n\nDefault target is always either one of:\n - The last creature hovered by the mouse\n - The last attacked creature\n - The closest creature"}
 	list[#list+1] = { zone=zone, name=string.toTString"#GOLD##{bold}#Auto-accept target#WHITE##{normal}#", status=function(item)
-		return tostring(config.settings.auto_accept_target and "disabled" or "enabled")
+		return tostring(config.settings.auto_accept_target and "enabled" or "disabled")
 	end, fct=function(item)
 		config.settings.auto_accept_target = not config.settings.auto_accept_target
 		game:saveSettings("auto_accept_target", ("auto_accept_target = %s\n"):format(tostring(config.settings.auto_accept_target)))
