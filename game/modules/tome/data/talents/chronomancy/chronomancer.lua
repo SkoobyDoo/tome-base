19486953e0f90e15a1b679c6c4cb908d44ce164f
DarkGod <darkgod@net-core.org>
2015-03-24 10:51:45 +0100

    Make sure temporal warden clones can not self resurrrect

--- game/modules/tome/data/talents/chronomancy/chronomancer.lua
+++ game/modules/tome/data/talents/chronomancy/chronomancer.lua
@@ -320,7 +320,6 @@ makeParadoxClone = function(self, target, duration)
 	m.seen_by = nil
 	m.can_talk = nil
 	m.clone_on_hit = nil
-	m.self_resurrect = nil
 	m.escort_quest = nil
 	m.unused_talents = 0
 	m.unused_generics = 0
@@ -355,6 +354,8 @@ makeParadoxClone = function(self, target, duration)
 	
 	-- And finally, a bit of sanity in case anyone decides they should blow up the world..
 	if m.preferred_paradox and m.preferred_paradox > 600 then m.preferred_paradox = 600 end
+
+	m.self_resurrect = nil
 	
 	return m
 end
