ab7f79305021f2c0e1065ec312b46f4479b5446d
Samuel Wegner <samuelwegner@hotmail.com>
2015-09-30 18:33:17 -0500

    Fix Arcane Combat and Chronomancy talents

--- game/modules/tome/dialogs/talents/ChronomancyContingency.lua
+++ game/modules/tome/dialogs/talents/ChronomancyContingency.lua
@@ -66,6 +66,17 @@ You may select a spell for Contingency to automatically use when your life falls
 	self:setFocus(self.c_list)
 	self:setupUI()
 
+	self.key:addCommands{
+		__TEXTINPUT = function(c)
+			if c == '~' then
+				self:use(self.cur_item, "right")
+			end
+			if self.list and self.list.chars[c] then
+				self:use(self.list.chars[c])
+			end
+		end,
+	}
+	engine.interface.PlayerHotkeys:bindAllHotkeys(self.key, function(i) self:defineHotkey(i) end)
 	self.key:addBinds{
 		EXIT = function() game:unregisterDialog(self) end,
 	}
--- game/modules/tome/dialogs/talents/ChronomancyMatrix.lua
+++ game/modules/tome/dialogs/talents/ChronomancyMatrix.lua
@@ -80,6 +80,17 @@ You may select a chronomancy spell to Matrix, reducing the cooldown of that spel
 	self:setFocus(self.c_list)
 	self:setupUI()
 
+	self.key:addCommands{
+		__TEXTINPUT = function(c)
+			if c == '~' then
+				self:use(self.cur_item, "right")
+			end
+			if self.list and self.list.chars[c] then
+				self:use(self.list.chars[c])
+			end
+		end,
+	}
+	engine.interface.PlayerHotkeys:bindAllHotkeys(self.key, function(i) self:defineHotkey(i) end)
 	self.key:addBinds{
 		EXIT = function() game:unregisterDialog(self) end,
 	}
