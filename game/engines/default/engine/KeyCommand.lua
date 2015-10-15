01abf52477c724023637abcda22bfa412aeae32a
Alex Ksandra <alex0player@gmail.com>
2015-10-15 03:07:28 +0300

    fixed anymod handling

--- game/engines/default/engine/KeyCommand.lua
+++ game/engines/default/engine/KeyCommand.lua
@@ -81,7 +81,7 @@ function _M:receiveKey(sym, ctrl, shift, alt, meta, unicode, isup, key)
 			handled = true
 		end
 	elseif not isup and self.commands[sym] and self.commands[sym].plain then
-		self.commands[sym].plain(sym, ctrl, shift, alt, meta, unicode)
+		self.commands[sym].plain(sym, ctrl, shift, alt, meta, unicode, key)
 		handled = true
 	elseif not isup and self.commands[self.__DEFAULT] and self.commands[self.__DEFAULT].plain then
 		self.commands[self.__DEFAULT].plain(sym, ctrl, shift, alt, meta, unicode, key)
@@ -110,13 +110,19 @@ function _M:addCommand(sym, mods, fct, anymod)
 	if sym == self.__TEXTINPUT then return self:setTextInput(mods) end
 
 	self.commands[sym] = self.commands[sym] or {}
-	if not fct then
-		self.commands[sym].plain = mods
+	local plain = false
+	if not fct or not mods then
+		plain = true
+		self.commands[sym].plain = fct or mods
 	else
 		table.sort(mods)
 		self.commands[sym][table.concat(mods,',')] = fct
 	end
-	if anymod then self.commands[sym].anymod = true end
+	if anymod or plain then
+		self.commands[sym].anymod = true
+	else -- added a keycombo for a specific set of mods
+		self.commands[sym].anymod = false
+	end
 end
 
 --- Adds a key to be fully ignored
