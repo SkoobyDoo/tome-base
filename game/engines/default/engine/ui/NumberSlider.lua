1ce0746314af2cdd23026aa1524e10ada82aa305
Alex Ksandra <alex0player@gmail.com>
2015-10-15 04:22:24 +0300

    added a title to it

--- game/engines/default/engine/ui/NumberSlider.lua
+++ game/engines/default/engine/ui/NumberSlider.lua
@@ -20,11 +20,11 @@
 local class= require "engine.class"
 local Numberbox = require "engine.ui.Numberbox"
 local Focusable = require "engine.ui.Focusable"
-local Base = require "engine.ui.Base"
+local WithTitle = require "engine.ui.WithTitle"
 
 -- a slider with an integrated numberbox
 -- @classmod engine.ui.NumberSlider
-module(..., class.inherit(Base, Focusable))
+module(..., class.inherit(WithTitle, Focusable))
 
 function _M:init(t)
 	self.min = t.min or 0
@@ -36,14 +36,14 @@ function _M:init(t)
 	self.size = t.size
 	self.w = t.w
 
-	Base.init(self, t)
+	WithTitle.init(self, t)
 end
 
 function _M:generate()
 	self.mouse:reset()
 	self.key:reset()
 
-	self.w = self.w or self.size
+	self:generateTitle()
 	self.left = self:getUITexture("ui/border_hor_left.png")
 	self.middle = self:getUITexture("ui/border_hor_middle.png")
 	self.right = self:getUITexture("ui/border_hor_right.png")
@@ -55,37 +55,42 @@ function _M:generate()
 		number = self.value,
 		fct = function(v) self:onChange() end,
 	}
+
+	self.h = math.max(self.nbox.h, self.middle.h)
+	self.w = self.w or self.size + self.title_w
+	self.size = self.w - self.title_w
+
 	self.key:addBind("ACCEPT", function() self:onChange() end)
 	self.key:addCommands{
 		_UP = function() self.nbox:updateText(1) self:onChange() end,
 		_DOWN = function() self.nbox:updateText(-1) self:onChange() end,
+		_PAGEUP = function() self.nbox:updateText(self.step) self:onChange() end,
+		_PAGEDOWN = function() self.nbox:updateText(-self.step) self:onChange() end,
 	}
 	self.key.atLast = function(sym, ctrl, shift, alt, meta, unicode, isup, key) self.nbox.key:receiveKey(sym, ctrl, shift, alt, meta, unicode, isup, key) print("KEY", unicode) end
-	self.h = math.max(self.nbox.h, self.middle.h)
-	self.w = self.size
-	self:onChange()
 
-	self.mouse:registerZone(self.left.w, 0, self.w - self.left.w - self.right.w, self.h, function(...) self.nbox.mouse:delegate(...) end, nil, "box")
+	self.mouse:registerZone(self.title_w + self.left.w, 0, self.size - self.left.w - self.right.w, self.h, function(...) self.nbox.mouse:delegate(...) end, nil, "box")
 	self.nbox.mouse.delegate_offset_y = (self.h - self.nbox.h) / 2
 	-- wheeeeeeee
 	local wheelTable = {wheelup = 1, wheeldown = -1}
-	self.mouse:registerZone(0, 0, self.w, self.h, function(button, x, y, xrel, yrel, bx, by, event)
+	self.mouse:registerZone(self.title_w, 0, self.size, self.h, function(button, x, y, xrel, yrel, bx, by, event)
 		if event ~= "button" or not wheelTable[button] then return false end
 		self.nbox:updateText(wheelTable[button])
 		self:onChange()
 	end, {button=true})
 	-- clicking on arrows
 	local stepTable = {left = self.step, right = 1}
-	self.mouse:registerZone(0, 0, self.left.w, self.h, function(button, x, y, xrel, yrel, bx, by, event)
+	self.mouse:registerZone(self.title_w, 0, self.left.w, self.h, function(button, x, y, xrel, yrel, bx, by, event)
 		if event ~= "button" or not stepTable[button] then return false end
 		self.nbox:updateText(-stepTable[button])
 		self:onChange()
 	end, {button=true}, "left")
-	self.mouse:registerZone(self.w - self.right.w, 0, self.right.w, self.h, function(button, x, y, xrel, yrel, bx, by, event)
+	self.mouse:registerZone(self.title_w + self.size - self.right.w, 0, self.right.w, self.h, function(button, x, y, xrel, yrel, bx, by, event)
 		if event ~= "button" or not stepTable[button] then return false end
 		self.nbox:updateText(stepTable[button])
 		self:onChange()
 	end, {button=true}, "right")
+
 	self:onChange()
 end
 
@@ -102,21 +107,22 @@ function _M:onChange()
 	local delta = self.max - self.min
 	local shift = self.value - self.min
 	local prop = delta > 0 and shift / delta or 0.5
-	local xmin, xmax = self.left.w + halfw, self.w - self.right.w - halfw
+	local xmin, xmax = self.left.w + halfw, self.size - self.right.w - halfw
 	local nbx = xmin + (xmax - xmin) * prop
 	self.range = {nbx - halfw, nbx + halfw}
 	local offsety = (self.h - self.nbox.h) / 2
 
-	self.nbox.mouse.delegate_offset_x = self.range[1]
-	self.mouse:updateZone("left", 0, 0, self.range[1], self.h)
-	self.mouse:updateZone("right", self.range[2], 0, self.w - self.range[2], self.h)
-	self.mouse:updateZone("box", self.range[1], offsety, self.range[2] - self.range[1], self.h - 2 * offsety)
+	self.nbox.mouse.delegate_offset_x = self.title_w + self.range[1]
+	self.mouse:updateZone("left", self.title_w, 0, self.range[1], self.h)
+	self.mouse:updateZone("right", self.title_w + self.range[2], 0, self.w - self.range[2], self.h)
+	self.mouse:updateZone("box", self.title_w + self.range[1], offsety, self.range[2] - self.range[1], self.h - 2 * offsety)
 end
 
 function _M:display(x, y, nb_keyframes)
-	self:textureToScreen(self.left, x, y + (self.h - self.left.h) / 2)
-	self:textureToScreen(self.right, x + self.w - self.right.w, y + (self.h - self.right.h) / 2)
-	self.middle.t:toScreenFull(x + self.left.w, y, self.w - self.left.w - self.right.w, self.middle.h, self.middle.tw, self.middle.th)
-	self.nbox:display(x + self.range[1], y + (self.h - self.nbox.h) / 2, nb_keyframes)
+	self:displayTitle(x, y, nb_keyframes)
+	self:textureToScreen(self.left, x + self.title_w, y + (self.h - self.left.h) / 2)
+	self:textureToScreen(self.right, x + self.title_w + self.size - self.right.w, y + (self.h - self.right.h) / 2)
+	self.middle.t:toScreenFull(x + self.title_w + self.left.w, y, self.size - self.left.w - self.right.w, self.middle.h, self.middle.tw, self.middle.th)
+	self.nbox:display(x + self.title_w + self.range[1], y + (self.h - self.nbox.h) / 2, nb_keyframes)
 end
 
