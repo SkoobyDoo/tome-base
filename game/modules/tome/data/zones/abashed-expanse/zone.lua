a895435ff18dd6d5858feb42272e7ecd373b42be
HousePet <simonandrecurtis@gmail.com>
2015-10-24 10:43:37 +1030

    Icons are good.

--- game/modules/tome/data/zones/abashed-expanse/zone.lua
+++ game/modules/tome/data/zones/abashed-expanse/zone.lua
@@ -33,7 +33,7 @@ return {
 	max_material_level = 1,
 	no_level_connectivity = true,
 	force_controlled_teleport = true,
-	--projectile_speed_mod = 0.3,
+	projectile_speed_mod = 0.3,
 	no_autoexplore = true,
 	effects = {"EFF_ZONE_AURA_ABASHED"},
 
