c03d35aa2d7e5f6ccee2c3cc6140f20250d494bc
DarkGod <darkgod@net-core.org>
2016-02-13 18:51:27 +0100

    "attack_recurse" also works on archery.

--- game/modules/tome/class/Game.lua
+++ game/modules/tome/class/Game.lua
@@ -1695,7 +1695,7 @@ function _M:setupCommands()
 			print("===============")
 		end end,
 		[{"_g","ctrl"}] = function() if config.settings.cheat then
-			self:changeLevel(5, "orcs+slumbering-caves")
+			self:changeLevel(2, "orcs+primal-forest")
 do return end
 			local o = game.zone:makeEntity(game.level, "object", {subtype="steamsaw", random_object=true}, nil, true)
 			if o then
