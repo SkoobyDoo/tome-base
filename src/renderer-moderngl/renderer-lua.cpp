486bd67785552c0fe9dda5fc2b229341bcf11b4b
Alex Ksandra <alex0player@gmail.com>
2016-02-16 05:04:15 +0300

    hopefully renderer changes

--- src/renderer-moderngl/renderer-lua.cpp
+++ src/renderer-moderngl/renderer-lua.cpp
@@ -25,45 +25,79 @@ extern "C" {
 #include "renderer-moderngl/renderer-lua.h"
 }
 
+template<class T=DisplayObject>
+T* userdata_to_DO(lua_State *L, int index, const char *auxclass = nullptr) {
+	DisplayObject **ptr;
+	if (auxclass) {
+		ptr = reinterpret_cast<DisplayObject**>(auxiliar_checkclass(L, auxclass, index));
+	} else {
+		ptr = reinterpret_cast<DisplayObject**>(lua_touserdata(L, index));
+		if (!ptr) luaL_error(L, "invalid display object passed");
+	}
+	T* result = dynamic_cast<T*>(*ptr);
+	if (!result) luaL_error(L, "display object of wrong class");
+	return result;
+}
+
+
 /******************************************************************
  ** Generic
  ******************************************************************/
-#undef __KIND
-#undef __DISPLAY_OBJECT
-#define __KIND(f) gl_renderer_##f
-#define __DISPLAY_OBJECT RendererGL
-#include "renderer-lua.inc.hpp"
+static int gl_generic_color(lua_State *L)
+{
+	DisplayObject *c = userdata_to_DO(L, 1);
+	c->setColor(lua_tonumber(L, 2), lua_tonumber(L, 3), lua_tonumber(L, 4), lua_tonumber(L, 5));
+	return 0;
+}
 
-#undef __KIND
-#undef __DISPLAY_OBJECT
-#define __KIND(f) gl_container_##f
-#define __DISPLAY_OBJECT DORContainer
-#include "renderer-lua.inc.hpp"
+static int gl_generic_translate(lua_State *L)
+{
+	DisplayObject *c = userdata_to_DO(L, 1);
+	c->translate(lua_tonumber(L, 2), lua_tonumber(L, 3), lua_tonumber(L, 4), lua_toboolean(L, 5));
+	return 0;
+}
 
-#undef __KIND
-#undef __DISPLAY_OBJECT
-#define __KIND(f) gl_target_##f
-#define __DISPLAY_OBJECT DORTarget
-#include "renderer-lua.inc.hpp"
+static int gl_generic_rotate(lua_State *L)
+{
+	DisplayObject *c = userdata_to_DO(L, 1);
+	c->rotate(lua_tonumber(L, 2), lua_tonumber(L, 3), lua_tonumber(L, 4), lua_toboolean(L, 5));
+	return 0;
+}
 
-#undef __KIND
-#undef __DISPLAY_OBJECT
-#define __KIND(f) gl_vertexes_##f
-#define __DISPLAY_OBJECT DORVertexes
-#include "renderer-lua.inc.hpp"
+static int gl_generic_scale(lua_State *L)
+{
+	DisplayObject *c = userdata_to_DO(L, 1);
+	c->scale(lua_tonumber(L, 2), lua_tonumber(L, 3), lua_tonumber(L, 4), lua_toboolean(L, 5));
+	return 0;
+}
 
-#undef __KIND
-#undef __DISPLAY_OBJECT
-#define __KIND(f) gl_text_##f
-#define __DISPLAY_OBJECT DORText
-#include "renderer-lua.inc.hpp"
+static int gl_generic_reset_matrix(lua_State *L)
+{
+	DisplayObject *c = userdata_to_DO(L, 1);
+	c->resetModelMatrix();
+	return 0;
+}
+
+static int gl_generic_shown(lua_State *L)
+{
+	DisplayObject *c = userdata_to_DO(L, 1);
+	c->shown(lua_toboolean(L, 2));
+	return 0;
+}
+
+static int gl_generic_remove_from_parent(lua_State *L)
+{
+	DisplayObject *c = userdata_to_DO(L, 1);
+	c->removeFromParent();
+	return 0;
+}
 
 /******************************************************************
  ** Renderer
  ******************************************************************/
 static int gl_renderer_new(lua_State *L)
 {
-	RendererGL **r = (RendererGL**)lua_newuserdata(L, sizeof(RendererGL*));
+	DisplayObject **r = (RendererGL**)lua_newuserdata(L, sizeof(DisplayObject*));
 	auxiliar_setclass(L, "gl{renderer}", -1);
 
 	int w = screen->w / screen_zoom;
@@ -79,57 +113,30 @@ static int gl_renderer_new(lua_State *L)
 
 static int gl_renderer_free(lua_State *L)
 {
-	RendererGL **r = (RendererGL**)auxiliar_checkclass(L, "gl{renderer}", 1);
-	delete(*r);
+	RendererGL *r = userdata_to_DO<RendererGL>(L, 1, "gl{renderer}");
+	delete(r);
 	lua_pushnumber(L, 1);
 	return 1;
 }
 
 static int gl_renderer_zsort(lua_State *L)
 {
-	RendererGL **r = (RendererGL**)auxiliar_checkclass(L, "gl{renderer}", 1);
-	(*r)->zSorting(lua_toboolean(L, 2));
+	RendererGL *r = userdata_to_DO<RendererGL>(L, 1, "gl{renderer}");
+	r->zSorting(lua_toboolean(L, 2));
 	return 0;
 }
 
 static int gl_renderer_cutoff(lua_State *L)
 {
-	RendererGL **c = (RendererGL**)auxiliar_checkclass(L, "gl{renderer}", 1);
-	(*c)->cutoff(lua_tonumber(L, 2), lua_tonumber(L, 3), lua_tonumber(L, 4), lua_tonumber(L, 5));
+	RendererGL *r = userdata_to_DO<RendererGL>(L, 1, "gl{renderer}");
+	r->cutoff(lua_tonumber(L, 2), lua_tonumber(L, 3), lua_tonumber(L, 4), lua_tonumber(L, 5));
 	return 0;
 }
 
 static int gl_renderer_toscreen(lua_State *L)
 {
-	RendererGL **renderer = (RendererGL**)auxiliar_checkclass(L, "gl{renderer}", 1);
-	(*renderer)->toScreen();
-	return 0;
-}
-
-static int gl_renderer_add(lua_State *L)
-{
-	// We do not make any checks on the types, so the same method can be used for container & renderer and to add any kind of display object
-	RendererGL **c = (RendererGL**)lua_touserdata(L, 1);
-	DisplayObject **add = (DisplayObject**)lua_touserdata(L, 2);
-	(*c)->add(*add);	
-	(*add)->setLuaRef(luaL_ref(L, LUA_REGISTRYINDEX));
-	return 0;
-}
-
-static int gl_renderer_remove(lua_State *L)
-{
-	// We do not make any checks on the types, so the same method can be used for container & renderer and to add any kind of display object
-	RendererGL **c = (RendererGL**)lua_touserdata(L, 1);
-	DisplayObject **add = (DisplayObject**)lua_touserdata(L, 2);
-	(*c)->remove(*add);
-	return 0;
-}
-
-static int gl_renderer_clear(lua_State *L)
-{
-	// We do not make any checks on the types, so the same method can be used for container & renderer and to add any kind of display object
-	RendererGL **c = (RendererGL**)lua_touserdata(L, 1);
-	(*c)->clear();
+	RendererGL *r = userdata_to_DO<RendererGL>(L, 1, "gl{renderer}");
+	r->toScreen();
 	return 0;
 }
 
@@ -138,7 +145,7 @@ static int gl_renderer_clear(lua_State *L)
  ******************************************************************/
 static int gl_container_new(lua_State *L)
 {
-	DORContainer **c = (DORContainer**)lua_newuserdata(L, sizeof(DORContainer*));
+	DisplayObject **c = (DisplayObject**)lua_newuserdata(L, sizeof(DisplayObject*));
 	auxiliar_setclass(L, "gl{container}", -1);
 	*c = new DORContainer();
 	(*c)->setLuaState(L);
@@ -148,8 +155,8 @@ static int gl_container_new(lua_State *L)
 
 static int gl_container_free(lua_State *L)
 {
-	DORContainer **c = (DORContainer**)auxiliar_checkclass(L, "gl{container}", 1);
-	delete(*c);
+	DORContainer *c = userdata_to_DO<DORContainer>(L, 1, "gl{container}");
+	delete(c);
 	lua_pushnumber(L, 1);
 	return 1;
 }
@@ -157,27 +164,27 @@ static int gl_container_free(lua_State *L)
 static int gl_container_add(lua_State *L)
 {
 	// We do not make any checks on the types, so the same method can be used for container & renderer and to add any kind of display object
-	DORContainer **c = (DORContainer**)lua_touserdata(L, 1);
-	DisplayObject **add = (DisplayObject**)lua_touserdata(L, 2);
-	(*c)->add(*add);	
-	(*add)->setLuaRef(luaL_ref(L, LUA_REGISTRYINDEX));
+	DORContainer *c = userdata_to_DO<DORContainer>(L, 1);
+	DisplayObject *add = userdata_to_DO(L, 2);
+	c->add(add);	
+	add->setLuaRef(luaL_ref(L, LUA_REGISTRYINDEX));
 	return 0;
 }
 
 static int gl_container_remove(lua_State *L)
 {
 	// We do not make any checks on the types, so the same method can be used for container & renderer and to add any kind of display object
-	DORContainer **c = (DORContainer**)lua_touserdata(L, 1);
-	DisplayObject **add = (DisplayObject**)lua_touserdata(L, 2);
-	(*c)->remove(*add);
+	DORContainer *c = userdata_to_DO<DORContainer>(L, 1);
+	DisplayObject *add = userdata_to_DO(L, 2);
+	c->remove(add);
 	return 0;
 }
 
 static int gl_container_clear(lua_State *L)
 {
 	// We do not make any checks on the types, so the same method can be used for container & renderer and to add any kind of display object
-	DORContainer **c = (DORContainer**)lua_touserdata(L, 1);
-	(*c)->clear();
+	DORContainer *c = userdata_to_DO<DORContainer>(L, 1);
+	c->clear();
 	return 0;
 }
 
@@ -186,7 +193,7 @@ static int gl_container_clear(lua_State *L)
  ******************************************************************/
 static int gl_target_new(lua_State *L)
 {
-	DORTarget **c = (DORTarget**)lua_newuserdata(L, sizeof(DORTarget*));
+	DisplayObject **c = (DisplayObject**)lua_newuserdata(L, sizeof(DisplayObject*));
 	auxiliar_setclass(L, "gl{target}", -1);
 	int w = screen->w / screen_zoom;
 	int h = screen->h / screen_zoom;
@@ -204,33 +211,30 @@ static int gl_target_new(lua_State *L)
 
 static int gl_target_free(lua_State *L)
 {
-	DORTarget **c = (DORTarget**)auxiliar_checkclass(L, "gl{target}", 1);
-	delete(*c);
+	DORTarget *c = userdata_to_DO<DORTarget>(L, 1, "gl{target}");
+	delete(c);
 	lua_pushnumber(L, 1);
 	return 1;
 }
 
 static int gl_target_use(lua_State *L)
 {
-	// We do not make any checks on the types, so the same method can be used for target & renderer and to add any kind of display object
-	DORTarget **c = (DORTarget**)lua_touserdata(L, 1);
-	(*c)->use(lua_toboolean(L, 2));
+	DORTarget *c = userdata_to_DO<DORTarget>(L, 1, "gl{target}");
+	c->use(lua_toboolean(L, 2));
 	return 0;
 }
 
 static int gl_target_displaysize(lua_State *L)
 {
-	// We do not make any checks on the types, so the same method can be used for target & renderer and to add any kind of display object
-	DORTarget **c = (DORTarget**)lua_touserdata(L, 1);
-	(*c)->displaySize(lua_tonumber(L, 2), lua_tonumber(L, 3), lua_toboolean(L, 4));
+	DORTarget *c = userdata_to_DO<DORTarget>(L, 1, "gl{target}");
+	c->displaySize(lua_tonumber(L, 2), lua_tonumber(L, 3), lua_toboolean(L, 4));
 	return 0;
 }
 
 static int gl_target_clearcolor(lua_State *L)
 {
-	// We do not make any checks on the types, so the same method can be used for target & renderer and to add any kind of display object
-	DORTarget **c = (DORTarget**)lua_touserdata(L, 1);
-	(*c)->setClearColor(lua_tonumber(L, 2), lua_tonumber(L, 3), lua_tonumber(L, 4), lua_tonumber(L, 5));
+	DORTarget *c = userdata_to_DO<DORTarget>(L, 1, "gl{target}");
+	c->setClearColor(lua_tonumber(L, 2), lua_tonumber(L, 3), lua_tonumber(L, 4), lua_tonumber(L, 5));
 	return 0;
 }
 
@@ -239,7 +243,7 @@ static int gl_target_clearcolor(lua_State *L)
  ******************************************************************/
 static int gl_vertexes_new(lua_State *L)
 {
-	DORVertexes **v = (DORVertexes**)lua_newuserdata(L, sizeof(DORVertexes*));
+	DisplayObject **v = (DisplayObject**)lua_newuserdata(L, sizeof(DisplayObject*));
 	auxiliar_setclass(L, "gl{vertexes}", -1);
 	*v = new DORVertexes();
 	(*v)->setLuaState(L);
@@ -249,29 +253,28 @@ static int gl_vertexes_new(lua_State *L)
 
 static int gl_vertexes_free(lua_State *L)
 {
-	DORVertexes **v = (DORVertexes**)auxiliar_checkclass(L, "gl{vertexes}", 1);
-	delete(*v);
+	DORVertexes *v = userdata_to_DO<DORVertexes>(L, 1, "gl{vertexes}");
+	delete(v);
 	lua_pushnumber(L, 1);
 	return 1;
 }
 
 static int gl_vertexes_clear(lua_State *L)
 {
-	DisplayObject **d = (DisplayObject**)lua_touserdata(L, 1);
-	DORVertexes *v = dynamic_cast<DORVertexes*>(*d);
+	DORVertexes *v = userdata_to_DO<DORVertexes>(L, 1, "gl{vertexes}");
 	v->clear();
 	return 0;
 }
 
 static int gl_vertexes_quad(lua_State *L)
 {
-	DORVertexes **v = (DORVertexes**)auxiliar_checkclass(L, "gl{vertexes}", 1);
+	DORVertexes *v = userdata_to_DO<DORVertexes>(L, 1, "gl{vertexes}");
 	float x1 = lua_tonumber(L, 2);  float y1 = lua_tonumber(L, 3);  float u1 = lua_tonumber(L, 4);  float v1 = lua_tonumber(L, 5); 
 	float x2 = lua_tonumber(L, 6);  float y2 = lua_tonumber(L, 7);  float u2 = lua_tonumber(L, 8);  float v2 = lua_tonumber(L, 9); 
 	float x3 = lua_tonumber(L, 10); float y3 = lua_tonumber(L, 11); float u3 = lua_tonumber(L, 12); float v3 = lua_tonumber(L, 13); 
 	float x4 = lua_tonumber(L, 14); float y4 = lua_tonumber(L, 15); float u4 = lua_tonumber(L, 16); float v4 = lua_tonumber(L, 17); 
 	float r = lua_tonumber(L, 18); float g = lua_tonumber(L, 19); float b = lua_tonumber(L, 20); float a = lua_tonumber(L, 21);
-	(*v)->addQuad(
+	v->addQuad(
 		x1, y1, u1, v1, 
 		x2, y2, u2, v2, 
 		x3, y3, u3, v3, 
@@ -283,19 +286,19 @@ static int gl_vertexes_quad(lua_State *L)
 
 static int gl_vertexes_texture(lua_State *L)
 {
-	DORVertexes **v = (DORVertexes**)auxiliar_checkclass(L, "gl{vertexes}", 1);
+	DORVertexes *v = userdata_to_DO<DORVertexes>(L, 1, "gl{vertexes}");
 	texture_type *t = (texture_type*)auxiliar_checkclass(L, "gl{texture}", 2);
 	lua_pushvalue(L, 2);
-	(*v)->setTexture(t->tex, luaL_ref(L, LUA_REGISTRYINDEX));
+	v->setTexture(t->tex, luaL_ref(L, LUA_REGISTRYINDEX));
 
 	return 0;
 }
 
 static int gl_vertexes_shader(lua_State *L)
 {
-	DORVertexes **v = (DORVertexes**)auxiliar_checkclass(L, "gl{vertexes}", 1);
+	DORVertexes *v = userdata_to_DO<DORVertexes>(L, 1, "gl{vertexes}");
 	shader_type *shader = (shader_type*)lua_touserdata(L, 2);
-	(*v)->setShader(shader);
+	v->setShader(shader);
 	return 0;
 }
 
@@ -304,84 +307,85 @@ static int gl_vertexes_shader(lua_State *L)
  ******************************************************************/
 static int gl_text_new(lua_State *L)
 {
-	DORText **v = (DORText**)lua_newuserdata(L, sizeof(DORText*));
+	DisplayObject **v = (DisplayObject**)lua_newuserdata(L, sizeof(DisplayObject*));
+	DORText *t;
 	auxiliar_setclass(L, "gl{text}", -1);
-	*v = new DORText();
+	*v = t = new DORText();
 	(*v)->setLuaState(L);
 
 	font_type *f = (font_type*)auxiliar_checkclass(L, "sdl{font}", 1);
-	if (lua_isnumber(L, 2)) (*v)->setMaxWidth(lua_tonumber(L, 2));
-	(*v)->setNoLinefeed(lua_toboolean(L, 3));
+	if (lua_isnumber(L, 2)) t->setMaxWidth(lua_tonumber(L, 2));
+	t->setNoLinefeed(lua_toboolean(L, 3));
 
 	lua_pushvalue(L, 1);
-	(*v)->setFont(f, luaL_ref(L, LUA_REGISTRYINDEX));
+	t->setFont(f, luaL_ref(L, LUA_REGISTRYINDEX));
 
 	return 1;
 }
 
 static int gl_text_free(lua_State *L)
 {
-	DORText **v = (DORText**)auxiliar_checkclass(L, "gl{text}", 1);
-	delete(*v);
+	DORText *v = userdata_to_DO<DORText>(L, 1, "gl{text}");
+	delete(v);
 	lua_pushnumber(L, 1);
 	return 1;
 }
 
 static int gl_text_linefeed(lua_State *L)
 {
-	DORText **v = (DORText**)auxiliar_checkclass(L, "gl{text}", 1);
-	(*v)->setNoLinefeed(!lua_toboolean(L, 2));
+	DORText *v = userdata_to_DO<DORText>(L, 1, "gl{text}");
+	v->setNoLinefeed(!lua_toboolean(L, 2));
 
 	return 0;
 }
 
 static int gl_text_max_width(lua_State *L)
 {
-	DORText **v = (DORText**)auxiliar_checkclass(L, "gl{text}", 1);
-	(*v)->setMaxWidth(lua_tonumber(L, 2));
+	DORText *v = userdata_to_DO<DORText>(L, 1, "gl{text}");
+	v->setMaxWidth(lua_tonumber(L, 2));
 
 	return 0;
 }
 
 static int gl_text_max_lines(lua_State *L)
 {
-	DORText **v = (DORText**)auxiliar_checkclass(L, "gl{text}", 1);
-	(*v)->setMaxLines(lua_tonumber(L, 2));
+	DORText *v = userdata_to_DO<DORText>(L, 1, "gl{text}");
+	v->setMaxLines(lua_tonumber(L, 2));
 
 	return 0;
 }
 
 static int gl_text_text_color(lua_State *L)
 {
-	DORText **v = (DORText**)auxiliar_checkclass(L, "gl{text}", 1);
-	(*v)->setTextColor(lua_tonumber(L, 2), lua_tonumber(L, 3), lua_tonumber(L, 4), lua_tonumber(L, 5));
+	DORText *v = userdata_to_DO<DORText>(L, 1, "gl{text}");
+	v->setTextColor(lua_tonumber(L, 2), lua_tonumber(L, 3), lua_tonumber(L, 4), lua_tonumber(L, 5));
 
 	return 0;
 }
 
 static int gl_text_set(lua_State *L)
 {
-	DORText **v = (DORText**)auxiliar_checkclass(L, "gl{text}", 1);
-	(*v)->setText(luaL_checkstring(L, 2));
+	DORText *v = userdata_to_DO<DORText>(L, 1, "gl{text}");
+	v->setText(luaL_checkstring(L, 2));
 
 	return 0;
 }
 
 static int gl_text_stats(lua_State *L)
 {
-	DORText **v = (DORText**)auxiliar_checkclass(L, "gl{text}", 1);
+	DORText *v = userdata_to_DO<DORText>(L, 1, "gl{text}");
 
-	lua_pushnumber(L, (*v)->w);
-	lua_pushnumber(L, (*v)->h);
-	lua_pushnumber(L, (*v)->nb_lines);
+	lua_pushnumber(L, v->w);
+	lua_pushnumber(L, v->h);
+	lua_pushnumber(L, v->nb_lines);
 	return 3;
 }
 
 static int gl_text_shader(lua_State *L)
 {
-	DORText **v = (DORText**)auxiliar_checkclass(L, "gl{text}", 1);
+	DORText *v = userdata_to_DO<DORText>(L, 1, "gl{text}");
 	shader_type *shader = (shader_type*)lua_touserdata(L, 2);
-	(*v)->setShader(shader);
+	v->setShader(shader);
 	return 0;
 }
 
@@ -394,16 +398,16 @@ static const struct luaL_Reg gl_renderer_reg[] =
 {
 	{"__gc", gl_renderer_free},
 	{"zSort", gl_renderer_zsort},
-	{"shown", gl_renderer_shown},
-	{"color", gl_renderer_color},
-	{"resetMatrix", gl_renderer_reset_matrix},
-	{"translate", gl_renderer_translate},
-	{"rotate", gl_renderer_rotate},
-	{"scale", gl_renderer_scale},
-	{"removeFromParent", gl_renderer_remove_from_parent},
-	{"add", gl_renderer_add},
-	{"remove", gl_renderer_remove},
-	{"clear", gl_renderer_clear},
+	{"shown", gl_generic_shown},
+	{"color", gl_generic_color},
+	{"resetMatrix", gl_generic_reset_matrix},
+	{"translate", gl_generic_translate},
+	{"rotate", gl_generic_rotate},
+	{"scale", gl_generic_scale},
+	{"removeFromParent", gl_generic_remove_from_parent},
+	{"add", gl_container_add},
+	{"remove", gl_container_remove},
+	{"clear", gl_container_clear},
 	{"cutoff", gl_renderer_cutoff},
 	{"toScreen", gl_renderer_toscreen},
 	{NULL, NULL},
@@ -416,13 +420,13 @@ static const struct luaL_Reg gl_target_reg[] =
 	{"displaySize", gl_target_displaysize},
 	{"clearColor", gl_target_clearcolor},
 	{"clear", gl_vertexes_clear},
-	{"shown", gl_target_shown},
-	{"color", gl_target_color},
-	{"resetMatrix", gl_target_reset_matrix},
-	{"translate", gl_target_translate},
-	{"rotate", gl_target_rotate},
-	{"scale", gl_target_scale},
-	{"removeFromParent", gl_target_remove_from_parent},
+	{"shown", gl_generic_shown},
+	{"color", gl_generic_color},
+	{"resetMatrix", gl_generic_reset_matrix},
+	{"translate", gl_generic_translate},
+	{"rotate", gl_generic_rotate},
+	{"scale", gl_generic_scale},
+	{"removeFromParent", gl_generic_remove_from_parent},
 	{NULL, NULL},
 };
 
@@ -432,13 +436,13 @@ static const struct luaL_Reg gl_container_reg[] =
 	{"add", gl_container_add},
 	{"remove", gl_container_remove},
 	{"clear", gl_container_clear},
-	{"shown", gl_container_shown},
-	{"color", gl_container_color},
-	{"resetMatrix", gl_container_reset_matrix},
-	{"translate", gl_container_translate},
-	{"rotate", gl_container_rotate},
-	{"scale", gl_container_scale},
-	{"removeFromParent", gl_container_remove_from_parent},
+	{"shown", gl_generic_shown},
+	{"color", gl_generic_color},
+	{"resetMatrix", gl_generic_reset_matrix},
+	{"translate", gl_generic_translate},
+	{"rotate", gl_generic_rotate},
+	{"scale", gl_generic_scale},
+	{"removeFromParent", gl_generic_remove_from_parent},
 	{NULL, NULL},
 };
 
@@ -449,13 +453,13 @@ static const struct luaL_Reg gl_vertexes_reg[] =
 	{"texture", gl_vertexes_texture},
 	{"shader", gl_vertexes_shader},
 	{"clear", gl_vertexes_clear},
-	{"shown", gl_vertexes_shown},
-	{"color", gl_vertexes_color},
-	{"resetMatrix", gl_vertexes_reset_matrix},
-	{"translate", gl_vertexes_translate},
-	{"rotate", gl_vertexes_rotate},
-	{"scale", gl_vertexes_scale},
-	{"removeFromParent", gl_vertexes_remove_from_parent},
+	{"shown", gl_generic_shown},
+	{"color", gl_generic_color},
+	{"resetMatrix", gl_generic_reset_matrix},
+	{"translate", gl_generic_translate},
+	{"rotate", gl_generic_rotate},
+	{"scale", gl_generic_scale},
+	{"removeFromParent", gl_generic_remove_from_parent},
 	{NULL, NULL},
 };
 
@@ -470,13 +474,13 @@ static const struct luaL_Reg gl_text_reg[] =
 	{"linefeed", gl_text_linefeed},
 	{"shader", gl_text_shader},
 	{"clear", gl_vertexes_clear},
-	{"shown", gl_text_shown},
-	{"color", gl_text_color},
-	{"resetMatrix", gl_text_reset_matrix},
-	{"translate", gl_text_translate},
-	{"rotate", gl_text_rotate},
-	{"scale", gl_text_scale},
-	{"removeFromParent", gl_text_remove_from_parent},
+	{"shown", gl_generic_shown},
+	{"color", gl_generic_color},
+	{"resetMatrix", gl_generic_reset_matrix},
+	{"translate", gl_generic_translate},
+	{"rotate", gl_generic_rotate},
+	{"scale", gl_generic_scale},
+	{"removeFromParent", gl_generic_remove_from_parent},
 	{NULL, NULL},
 };
 
