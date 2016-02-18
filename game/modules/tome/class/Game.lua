5e5967ce6e538875e402d46934acf26cf995db46
DarkGod <darkgod@net-core.org>
2016-02-18 15:00:15 +0100

    Added shoulder attachement locations to all player dolls

--- game/modules/tome/class/Game.lua
+++ game/modules/tome/class/Game.lua
@@ -1697,8 +1697,8 @@ function _M:setupCommands()
 			print("===============")
 		end end,
 		[{"_g","ctrl"}] = function() if config.settings.cheat then
--- 			self:changeLevel(1, "orcs+palace-fumes")
--- do return end
+			self:changeLevel(6, "orcs+palace-fumes")
+do return end
 			local o = game.zone:makeEntity(game.level, "object", {subtype="sling", random_object=true}, nil, true)
 			if o then
 				o:identify(true)
