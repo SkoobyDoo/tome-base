f8dcba97adfcd35da82f6d16841260e424ba8d7b
DarkGod <darkgod@net-core.org>
2017-01-30 00:14:04 +0100

    When the player enters a level (the first time only), any nearby hostiles in radius 4 are scattered around the level, to prevent entering deathtraps

--- game/modules/tome/class/Player.lua
+++ game/modules/tome/class/Player.lua
@@ -208,7 +208,14 @@ function _M:onEnterLevel(zone, level)
 end
 
 function _M:onEnterLevelEnd(zone, level)
-
+	if level._player_enter_scatter then return end
+	level._player_enter_scatter = true
+	self:project({type="ball", radius=4}, self.x, self.y, function(px, py)
+		local a = level.map(px, py, Map.ACTOR)
+		if a and self:reactionToward(a) < 0 then
+			a:teleportRandom(self.x, self.y, 50, 5)
+		end
+	end)
 end
 
 function _M:onLeaveLevel(zone, level)
