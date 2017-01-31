9ffee0d496c33f8342829d059deda3fc337f0bea
DarkGod <darkgod@net-core.org>
2017-01-31 12:38:39 +0100

    New quest popup art for Infinite Dungeon Challenges

--- game/modules/tome/class/GameState.lua
+++ game/modules/tome/class/GameState.lua
@@ -2532,6 +2532,7 @@ function _M:makeChallengeQuest(level, name, desc, data, alter_effect)
 	local q = {
 		id = "id-challenge-"..level.level,
 		name = "Infinite Dungeon Challenge: "..name.." (Level "..level.level..")",
+		use_ui = "quest-idchallenge",
 		challenge_desc = desc,
 		desc = function(self, who)
 			local desc = {}
--- game/modules/tome/data/gfx/ui/definitions/quest.lua
+++ game/modules/tome/data/gfx/ui/definitions/quest.lua
@@ -36,3 +36,13 @@ def['quest-fail'] = {
 	frame_oy2 =  60,
 	dialog_h_middles = true,
 }
+
+def['quest-idchallenge'] = {
+	frame_alpha = 1,
+	frame_darkness = 0.6,
+	frame_ox1 = -60,
+	frame_ox2 =  60,
+	frame_oy1 = -154,
+	frame_oy2 =  60,
+	dialog_h_middles = true,
+}
