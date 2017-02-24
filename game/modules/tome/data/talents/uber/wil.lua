28ea12c0e8b5e393c55ce7c61da9d7c753f2ac89
DarkGod <darkgod@net-core.org>
2017-02-24 16:34:40 +0100

    Garkul's Revenge also increases damage to giants

--- game/modules/tome/data/talents/uber/wil.lua
+++ game/modules/tome/data/talents/uber/wil.lua
@@ -130,10 +130,12 @@ uberTalent{
 		self.inc_damage_actor_type = self.inc_damage_actor_type or {}
 		self.inc_damage_actor_type.construct = (self.inc_damage_actor_type.construct or 0) + 1000
 		self.inc_damage_actor_type.humanoid = (self.inc_damage_actor_type.humanoid or 0) + 20
+		self.inc_damage_actor_type.humanoid = (self.inc_damage_actor_type.giant or 0) + 20
 	end,
 	on_unlearn = function(self, t)
 		self.inc_damage_actor_type.construct = (self.inc_damage_actor_type.construct or 0) - 1000
 		self.inc_damage_actor_type.humanoid = (self.inc_damage_actor_type.humanoid or 0) - 20
+		self.inc_damage_actor_type.humanoid = (self.inc_damage_actor_type.giant or 0) - 20
 	end,
 	require = { special={desc="Possess and wear two of Garkul's artifacts and know all about Garkul's life", fct=function(self)
 		local o1 = self:findInAllInventoriesBy("define_as", "SET_GARKUL_TEETH")
@@ -147,7 +149,7 @@ uberTalent{
 			))
 	end} },
 	info = function(self, t)
-		return ([[Garkul's spirit is with you. You now deal 1000%% more damage to constructs and 20%% more damage to humanoids.]])
+		return ([[Garkul's spirit is with you. You now deal 1000%% more damage to constructs and 20%% more damage to humanoids and giants.]])
 		:format()
 	end,
 }
