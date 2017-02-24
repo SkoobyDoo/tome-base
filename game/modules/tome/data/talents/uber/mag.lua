57568f89db8a906aecf2870517080b390619d89e
DarkGod <darkgod@net-core.org>
2017-02-24 16:23:21 +0100

    Aether Permeation also gives 10% arcane resist cap

--- game/modules/tome/data/talents/uber/mag.lua
+++ game/modules/tome/data/talents/uber/mag.lua
@@ -47,13 +47,14 @@ uberTalent{
 		self:talentTemporaryValue(ret, "force_use_resist", DamageType.ARCANE)
 		self:talentTemporaryValue(ret, "force_use_resist_percent", 66)
 		self:talentTemporaryValue(ret, "resists", {[DamageType.ARCANE] = 20})
+		self:talentTemporaryValue(ret, "resists_cap", {[DamageType.ARCANE] = 10})
 		return ret
 	end,
 	on_unlearn = function(self, t)
 	end,
 	info = function(self, t)
 		return ([[You manifest a thin layer of aether all around you. Any attack passing through it will check arcane resistance instead of the incoming damage resistance.
-		In effect, all of your resistances are equal to 66%% of your arcane resistance, which is increased by 20%%.]])
+		In effect, all of your resistances are equal to 66%% of your arcane resistance, which is increased by 20%% (and cap increased by 10%%).]])
 		:format()
 	end,
 }
