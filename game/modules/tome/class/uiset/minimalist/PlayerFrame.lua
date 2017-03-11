62ff4d06f7e666aa128947949cd87aa020c9e807
DarkGod <darkgod@net-core.org>
2017-03-11 13:41:26 +0100

    mip

--- game/modules/tome/class/uiset/minimalist/PlayerFrame.lua
+++ game/modules/tome/class/uiset/minimalist/PlayerFrame.lua
@@ -178,17 +178,18 @@ function _M:getPlayer()
 end
 
 function _M:getPlayerDO()
-	-- We dont use Entity:getEntityDisplayObject because we need no caching
 	local tiles = Map.tiles
 	if game.level and game.level.map then tiles = game.level.map.tiles end
+	return self:getPlayer():getEntityDisplayObject(tiles, 40, 40, 1, false, true, true)
+	-- We dont use Entity:getEntityDisplayObject because we need no caching
 
-	local mos = {}
-	local list = {}
-	self:getPlayer():getMapObjects(tiles, mos, 1)
-	for i = 1, Map.zdepth do
-		if mos[i] then list[#list+1] = mos[i] end
-	end
-	return core.map.mapObjectsToDisplayObject(40, 40, 1, false, true, unpack(list))
+	-- local mos = {}
+	-- local list = {}
+	-- self:getPlayer():getMapObjects(tiles, mos, 1)
+	-- for i = 1, Map.zdepth do
+	-- 	if mos[i] then list[#list+1] = mos[i] end
+	-- end
+	-- return core.map.mapObjectsToDisplayObject(40, 40, 1, false, true, unpack(list))
 end
 
 function _M:move(x, y)
