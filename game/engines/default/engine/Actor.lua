06f33e7fedefce3cacb10f7e56c57c9c68e7f171
Hachem_Muche <Hachem_Muche@stanfordalumni.org>
2017-03-30 14:15:03 -0700

    Persistent debugging code

--- game/engines/default/engine/Actor.lua
+++ game/engines/default/engine/Actor.lua
@@ -90,7 +90,7 @@ _M.clone_copy = nil
 function _M:cloneActor(post_copy, alt_nodes)
 	alt_nodes = table.merge(alt_nodes or {}, self.clone_nodes, true)
 	if post_copy or self.clone_copy then post_copy = post_copy or {} table.update(post_copy, self.clone_copy or {}, true) end
-	local a = self:cloneCustom(alt_nodes, post_copy), post_copy
+	local a = self:cloneCustom(alt_nodes, post_copy)
 	a:removeAllMOs()
 	return a, post_copy
 end
