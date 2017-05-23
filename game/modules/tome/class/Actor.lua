ebaa5f7f4f7a560204fbe6733de5fd0860a5f6fa
DarkGod <darkgod@net-core.org>
2017-05-23 21:42:20 +0200

    Fixed shops

--- game/modules/tome/class/Actor.lua
+++ game/modules/tome/class/Actor.lua
@@ -1648,10 +1648,10 @@ function _M:reactionToward(target, no_reflection)
 	if rsrc == target and self ~= target and target:attr("encased_in_ice") then return -50 end  -- summons shouldn't hate each other and shouldn't hate summoner more than enemies
 
 	-- Neverending hatred
-	if rtarget:attr("hated_by_everybody") and rtarget ~= rsrc then return -100 end
-	if rsrc:attr("hates_everybody") and rtarget ~= rsrc then return -100 end
-	if rsrc:attr("hates_arcane") and rtarget:attr("has_arcane_knowledge") and not rtarget:attr("forbid_arcane") then return -100 end
-	if rsrc:attr("hates_antimagic") and rtarget:attr("forbid_arcane") then return -100 end
+	if rtarget.attr and rtarget:attr("hated_by_everybody") and rtarget ~= rsrc then return -100 end
+	if rsrc.attr and rsrc:attr("hates_everybody") and rtarget ~= rsrc then return -100 end
+	if rsrc.attr and rtarget.attr and rsrc:attr("hates_arcane") and rtarget:attr("has_arcane_knowledge") and not rtarget:attr("forbid_arcane") then return -100 end
+	if rsrc.attr and rtarget.attr and rsrc:attr("hates_antimagic") and rtarget:attr("forbid_arcane") then return -100 end
 
 	local v = engine.Actor.reactionToward(rsrc, rtarget)
 
