d51515fcd47ebc6d37887aa91a578c9d69ed5da8
DarkGod <darkgod@net-core.org>
2018-04-05 20:23:40 +0200

    Fixed Fortress and Glass walls for corner cases

--- game/modules/tome/data/general/grids/fortress.lua
+++ game/modules/tome/data/general/grids/fortress.lua
@@ -36,7 +36,7 @@ newEntity{
 	name = "wall", image = "terrain/solidwall/solid_wall_block1.png",
 	display = '#', color_r=255, color_g=255, color_b=255, back_color=colors.GREY,
 	z = 3,
-	nice_tiler = { method="wall3d", inner={"SOLID_WALL", 100, 1, 1}, north={"SOLID_WALL_NORTH", 100, 1, 1}, south={"SOLID_WALL_SOUTH", 10, 1, 7}, north_south="SOLID_WALL_NORTH_SOUTH",  },
+	nice_tiler = { method="wall3d", inner={"SOLID_WALL", 100, 1, 1}, north={"SOLID_WALL_NORTH", 100, 1, 1}, south={"SOLID_WALL_SOUTH", 10, 1, 7}, north_south="SOLID_WALL_NORTH_SOUTH", pillar_8={"SOLID_WALL_PILLAR_8", 100, 1, 1}, pillar_2="SOLID_WALL_PILLAR_2", pillar_6="SOLID_WALL_NORTH_SOUTH", pillar_4="SOLID_WALL_NORTH_SOUTH" },
 	always_remember = true,
 	does_block_move = true,
 	block_sight = true,
@@ -45,7 +45,9 @@ newEntity{
 for i = 1, 1 do
 	newEntity{ base = "SOLID_WALL", define_as = "SOLID_WALL"..i, image = "terrain/solidwall/solid_wall_block"..i..".png", z = 3}
 	newEntity{ base = "SOLID_WALL", define_as = "SOLID_WALL_NORTH"..i, image = "terrain/solidwall/solid_wall_block"..i..".png", z = 3, add_displays = {class.new{image="terrain/solidwall/solid_wall_top_block1.png", z=18, display_y=-1}}}
+	newEntity{ base = "SOLID_WALL", define_as = "SOLID_WALL_PILLAR_8"..i, image = "terrain/solidwall/solid_wall_block"..i..".png", z = 3, add_displays = {class.new{image="terrain/solidwall/solid_wall_top_block1.png", z=18, display_y=-1}}}
 end
+newEntity{ base = "SOLID_WALL", define_as = "SOLID_WALL_PILLAR_2", image = "terrain/solidwall/solid_wall1.png", z = 3}
 newEntity{ base = "SOLID_WALL", define_as = "SOLID_WALL_NORTH_SOUTH", image = "terrain/solidwall/solid_wall1.png", z = 3, add_displays = {class.new{image="terrain/solidwall/solid_wall_top_block1.png", z=18, display_y=-1}}}
 newEntity{ base = "SOLID_WALL", define_as = "SOLID_WALL_SOUTH", image = "terrain/solidwall/solid_wall1.png", z = 3}
 for i = 1, 7 do newEntity{ base = "SOLID_WALL", define_as = "SOLID_WALL_SOUTH"..i, image = "terrain/solidwall/solid_wall"..i..".png", z = 3} end
