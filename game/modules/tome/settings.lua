0c9df86cb53407d382cdfeac1648f2d98a4e2896
DarkGod <darkgod@net-core.org>
2018-04-22 23:07:34 +0200

    frell me

--- game/modules/tome/settings.lua
+++ game/modules/tome/settings.lua
@@ -58,6 +58,7 @@ if type(config.settings.tome.show_grid_lines) == "nil" then config.settings.tome
 if type(config.settings.tome.tinker_auto_switch) == "nil" then config.settings.tome.tinker_auto_switch = true end
 if type(config.settings.tome.quest_popup) == "nil" then config.settings.tome.quest_popup = true end
 if type(config.settings.tome.show_cloak_hoods) == "nil" then config.settings.tome.show_cloak_hoods = false end
+if type(config.settings.tome.upload_charsheet) == "nil" then config.settings.tome.upload_charsheet = true end
 if not config.settings.tome.fonts then config.settings.tome.fonts = {type="fantasy", size="normal"} end
 if not config.settings.tome.ui_theme3 then config.settings.tome.ui_theme3 = "dark" end
 if not config.settings.tome.uiset_mode then config.settings.tome.uiset_mode = "Minimalist" end
