40361e23b4d5777f5dbe0d31d86e1d02304794c3
DarkGod <darkgod@net-core.org>
2018-04-23 01:53:02 +0200

    FUCKING STUPID
    * Fixed error on changing level after a version upgrade. Stupid me sorry :/

--- game/modules/tome/class/GameState.lua
+++ game/modules/tome/class/GameState.lua
@@ -2461,6 +2461,7 @@ function _M:startEvents()
 		for i, e in ipairs(game.zone.assigned_events[game.level.level] or {}) do
 			local f, err = loadfile(self:eventBaseName("", e))
 			if not f then error(err) end
+			game.zone.assigned_events[game.level.level].params = game.zone.assigned_events[game.level.level].params or {}
 			setfenv(f, setmetatable({level=game.level, zone=game.zone, event_id=e.name, params=game.zone.assigned_events[game.level.level].params[i], Map=Map}, {__index=_G}))
 			f()
 		end
